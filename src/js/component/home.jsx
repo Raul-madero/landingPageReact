01da0bb6117d5fd58423e70c07da87cf8bb02ba9
Raul Madero <r.madero.ramirez@gmail.com>
2023-11-02 21:58:49 -0600

    Se agregó el navbar

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -4,23 +4,44 @@ import React from "react";
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
-const Home = () => {
+const NavBar = () => {
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
+		<nav className="navbar bg-secondary d-flex justify-content-between">
+			<div className="container-fluid w-25 m-0">
+				<a className="navbar-brand" href="#">Navbar</a>
+			</div>
+			<div>
+				<ul className="nav me-2 mb-2 d-flex align-items-center">
+					<li className="nav-item">
+						<a className="nav-link text-black" aria-current="page" href="#">Home</a>
+					</li>
+					<li className="nav-item">
+						<a className="nav-link text-black" aria-current="page" href="#">About us</a>
+					</li>
+					<li className="nav-item">
+						<a className="nav-link text-black" aria-current="page" href="#">Info</a>
+					</li>
+					<li className="nav-item">
+						<a className="nav-link text-black" aria-current="page" href="#">Contact</a>
+					</li>
+				</ul>
+			</div>
+		</nav>
 	);
 };
-
+const Jumbotron = () => {
+	return (
+		<div className="container-lg">Adios</div>
+	);
+};
+const Main = () => {
+	return (
+		<Jumbotron />
+	)
+};
+const Home = () => {
+	return (
+		<NavBar />
+	);
+};
 export default Home;
